3814c8b3684064791bd9485e328a1aeffef617c3
hanati <hanati@tezign.com>
2019-11-18 18:15:44 +0800

    first commit

---
+++ loss_network.py
@@ -0,0 +1,143 @@
+import torch
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+
+import vgg
+
+
+def vgg19(vgg_path, pretrained=True, **kwargs):
+    """VGG 19-layer model (configuration "E")
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    if pretrained:
+        kwargs['init_weights'] = False
+    model = vgg.VGG(vgg.make_layers(vgg.cfg['E']), **kwargs)
+    if pretrained:
+        state_dict = torch.load(vgg_path)
+        state_dict = {k:v for k, v in state_dict.items() if 'class' not in k}
+        model.load_state_dict(state_dict)
+    return model
+
+
+class LossNet(nn.Module):
+    def __init__(self, vgg_path):
+        super(LossNet, self).__init__()
+        model_list = list(vgg19(vgg_path).features.children())
+        self.conv1_1 = model_list[0]
+        self.conv1_2 = model_list[2]
+        self.conv2_1 = model_list[5]
+        self.conv2_2 = model_list[7]
+        self.conv3_1 = model_list[10]
+        self.conv3_2 = model_list[12]
+        self.conv3_3 = model_list[14]
+        self.conv3_4 = model_list[16]
+        self.conv4_1 = model_list[19]
+        self.conv4_2 = model_list[21]
+        self.conv4_3 = model_list[23]
+        self.conv4_4 = model_list[25]
+        self.conv5_1 = model_list[28]
+        self.conv5_2 = model_list[30]
+        self.conv5_3 = model_list[32]
+        self.conv5_4 = model_list[34]
+
+    def forward(self, x, out_key):
+        out = {}
+        out['conv1_1'] = F.relu(self.conv1_1(x))
+        out['conv1_2'] = F.relu(self.conv1_2(out['conv1_1']))
+        out['pool1']   = F.max_pool2d(out['conv1_2'])
+
+        out['conv2_1'] = F.relu(self.conv2_1(out['pool1']))
+        out['conv2_2'] = F.relu(self.conv2_2(out['conv2_1']))
+        out['pool2']   = F.max_pool2d(out['conv2_2'])
+        
+        out['conv3_1'] = F.relu(self.conv3_1(out['pool2']))
+        out['conv3_2'] = F.relu(self.conv3_2(out['conv3_1']))
+        out['conv3_3'] = F.relu(self.conv3_3(out['conv3_2']))
+        out['conv3_4'] = F.relu(self.conv3_4(out['conv3_3']))
+        out['pool3']   = F.max_pool2d(out['conv3_4'])
+
+        out['conv4_1'] = F.relu(self.conv4_1(out['pool3']))
+        out['conv4_2'] = F.relu(self.conv4_2(out['conv4_1']))
+        out['conv4_3'] = F.relu(self.conv4_3(out['conv4_2']))
+        out['conv4_4'] = F.relu(self.conv4_4(out['conv4_3']))
+        out['pool4']   = F.max_pool2d(out['conv4_4'])
+
+        out['conv5_1'] = F.relu(self.conv5_1(out['pool4']))
+        out['conv5_2'] = F.relu(self.conv5_2(out['conv5_1']))
+        out['conv5_3'] = F.relu(self.conv5_3(out['conv5_2']))
+        out['conv5_4'] = F.relu(self.conv5_4(out['conv5_3']))
+        out['pool5']   = F.max_pool2d(out['conv5_4'])
+
+        return [out[key] for key in out_key]
+
+
+class ContentLoss(nn.Module):
+    def __init__(self, gpu):
+        if gpu:
+            loss = nn.MSELoss().cuda()
+        else:
+            loss = nn.MSELoss()
+        self.loss = loss
+
+    def forward(self, x, target):
+        assert x.shape == target.shape, "input & target shape ain't same."
+        b, c, h, w = x.shape
+
+        return (1 /(c * h * w)) * torch.mean((x - target) ** 2)
+
+class StyleLoss(nn.Module):
+    def __init__(self, gpu):
+        if gpu:
+            loss = nn.MSELoss().cuda()
+        else:
+            loss = nn.MSELoss()
+        self.loss = loss
+    def forward(self, x, target):
+        channel = x.shape[3]
+        return (1 / channel ** 2) * self.loss(GramMatrix()(x), GramMatrix()(target))
+
+class TemporalLoss(nn.Module):
+    """
+    x: frame t 
+    f_x1: optical flow(frame t-1)
+    cm: confidence mask of optical flow 
+    """
+    def __init__(self, gpu):
+        if gpu:
+            loss = nn.MSELoss().cuda()
+        else:
+            loss = nn.MSELoss()
+        self.loss = loss
+
+    def forward(self, x, f_x1, cm):
+        assert x.shape == f_x1, "inputs are ain't same"
+        x = x.view(1, -1)
+        f_x1 = f_x1.view(1, -1)
+        cm = cm.view(-1)
+
+        D = f_x1.shape[1]
+        return (1 / D) * cm * x, f_x1
+
+class TVLoss(nn.Module):
+    def forward(self, x):
+        b, c, h, w = x.shape
+        
+        sum = 0
+        for i_c in range(c):
+            for i_h in range(h-1):
+                for i_w in range(w-1):
+                    sum += (x[0][i_c][i_h][i_w+1] - x[0][i_c][i_h][i_w]) ** 2
+                    sum += (x[0][i_c][i_h+1][i_w] - x[0][i_c][i_h][i_w]) ** 2
+
+        return sum ** 0.5
+                    
+class GramMatrix(nn.Module):
+    def forward(self, x):
+        a, b, c, d = x.shape
+        features = x.view(a * b, c * d)
+        G = torch.mm(features, features.t())
+        return G.div(a * b * c * d)
+
+
---
+++ transfer.py
@@ -0,0 +1,106 @@
+import torch
+import torch.optim as optim
+
+from torch.autograd import Variable
+from torchvision import datasets, transforms
+from PIL import Image
+
+from style_network import *
+from loss_network import *
+from dataset import get_loader
+from opticalflow import opticalflow
+
+
+class Transfer:
+    def __init__(self, epoch, data_path, style_path, vgg_path, lr, spatial_a, spatial_b, spatial_r, temporal_lambda, gpu=False, img_shape=(640, 360)):
+        self.epoch = epoch
+        self.data_path = data_path
+        self.style_path = style_path
+        self.lr = lr
+        self.gpu = gpu
+
+        self.s_a = spatial_a
+        self.s_b = spatial_b
+        self.s_r = spatial_r 
+        self.t_l = temporal_lambda
+
+        self.style_net = StyleNet()
+        self.loss_net = LossNet(vgg_path)
+        self.style_layer = ['conv1_2', 'conv2_2', 'conv3_2', 'conv4_2']
+
+        self.transform = transforms.Compose([transforms.ToTensor(),
+                                             transforms.Normalize((0.485, 0.456, 0.406),
+                                                                  (0.229, 0.224, 0.225))])
+        self.img_shape = img_shape                                                                  
+
+    def load_style(self):
+        transform = transforms.Compose([transforms.ToTensor()])
+
+        img = Image.open(self.style_path)
+        img = img.resize(self.img_shape)
+        img = transform(img).float()
+        img = Variable(img, requires_grad=True)
+        return img
+
+    def train(self):        
+        style_img = self.load_style()
+
+        if self.gpu:
+            self.style_net = self.style_net.gpu()
+            self.loss_net = self.loss_net.gpu()
+            style_img = style_img.gpu()
+
+        adam = optim.Adam(self.style_net.parameters(), lr=self.lr)
+        
+        loader = get_loader(1, self.data_path, self.img_shape, self.transform)
+        print('Data Load Success!!')
+
+        print('Training Start!!')
+        for count in range(self.epoch):
+            for step, frames in enumerate(loader):
+                for i in range(1, len(frames)):
+                    x_t = frames[i]
+                    x_t1 = frames[i-1]
+
+                    h_xt = self.style_net(x_t)
+                    h_xt1 = self.style_net(x_t1)
+
+                    s_xt = self.loss_net(x_t, self.style_layer)
+                    s_xt1 = self.loss_net(x_t1, self.style_layer)
+                    s_hxt = self.loss_net(h_xt, self.style_layer)
+                    s_hxt1 = self.loss_net(h_xt1, self.style_layer)
+                    s = self.loss_net(style_img, self.style_layer)
+
+                    # ContentLoss, conv4_2
+                    content_t = ContentLoss(self.gpu)(s_xt[3], s_hxt[3])
+                    content_t1 = ContentLoss(self.gpu)(s_xt1[3], s_hxt1[3])
+                    content_loss = content_t + content_t1
+
+                    # StyleLoss
+                    style_t = None
+                    style_t1 = None
+                    tv_loss = None
+                    for layer in range(len(self.style_layer)):
+                        style_t += StyleLoss(self.gpu)(s, s_hxt[layer])
+                        style_t1 += StyleLoss(self.gpu)(s, s_hxt1[layer])
+
+                        # TVLoss
+                        tv_loss = TVLoss(s_hxt[layer])
+                    style_loss = style_t + style_t1
+
+                    # Optical flow
+                    flow, mask = opticalflow(h_xt.data.numpy(), h_xt1.data.numpy())
+
+                    # Temporal Loss
+                    temporal_loss = TemporalLoss(self.gpu)(h_xt, flow, mask)
+                    # Spatial Loss
+                    spatial_loss = self.s_a * content_loss + self.s_b * style_loss + self.s_r * tv_loss
+                    
+                    Loss = spatial_loss + self.t_l * temporal_loss
+                    Loss.backward(retain_graph=True)
+                    adam.step()
+
+
+
+transfer = Transfer(10, 'data', 'data/1.jpeg', 'model/vgg19-dcbb9e9d.pth', 0.1, 0.3, 0.3, 0.1, 0.2, gpu=False, img_shape=(1920, 1080))
+transfer.train()
