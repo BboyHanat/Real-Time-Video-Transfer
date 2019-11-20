dc7d001918ba23321ca3e47aa902b74d2547c626
hanati <hanati@tezign.com>
2019-11-20 16:30:41 +0800

    change show step

--- transfer.py
+++ transfer.py
@@ -141,6 +141,7 @@ class Transfer:
                         np_image = h_xt.data.cpu().numpy()
                         np_image = np.squeeze(np.transpose(np_image, (0, 2, 3, 1)))
                         transform_np = (np_image * (0.229, 0.224, 0.225) + (0.485, 0.456, 0.406)) * 255
+                        transform_np = transform_np.clip(0, 255)
                         np_image = np.asarray(transform_np, np.uint8)
 
                         cv2.imwrite('output/style_e{}_s{}_i{}.jpg'.format(count, step, i), np_image)
